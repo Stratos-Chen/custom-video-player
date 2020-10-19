33fbf1f53b23c09b81650bf32b4be502d01211c4
“BANDER <Bandersaleh93@gmail.com>
2020-10-19 16:37:15 -0700

    Initial Filter Update

--- scripts.js
+++ scripts.js
@@ -1,28 +1,59 @@
-/*Get our elements */
-const player = document.querySelector(".player");
-const video = player.querySelector(".viewer");
-const progress = player.querySelector(".progress");
-const progressBar = player.querySelector(".progress__filled");
-const toggle = player.querySelector(".toggle");
-const skipButtons = player.querySelectorAll("[data-skip]");
-const ranges = player.querySelectorAll(".player__slider");
+/* Get Our Elements */
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+
 
 /* Build out functions */
 function togglePlay() {
-  if (video.paused) {
-    video.play();
-  } else {
-    video.pause();
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
+}
+
+function filterButton() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  player.width = width;
+  player.height = height;
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+    // take the pixels out
+    let pixels = ctx.getImageData(0, 0, width, height);
+    // mess with them
+    // pixels = redEffect(pixels);
+
+    pixels = rgbSplit(pixels);
+    // ctx.globalAlpha = 0.8;
+
+    // pixels = greenScreen(pixels);
+    // put them back
+    ctx.putImageData(pixels, 0, 0);
+  }, 16);
+}
+
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i+=4) {
+    pixels.data[i - 150] = pixels.data[i + 0]; // RED
+    pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
+    pixels.data[i - 550] = pixels.data[i + 2]; // Blue
   }
+  return pixels;
 }
 
 function updateButton() {
-  const icon = this.paused ? "►" : "❚ ❚";
+  const icon = this.paused ? '►' : '❚ ❚';
+  console.log(icon);
   toggle.textContent = icon;
 }
 
 function skip() {
-  video.currentTime += parseFloat(this.dataset.skip);
+ video.currentTime += parseFloat(this.dataset.skip);
 }
 
 function handleRangeUpdate() {
@@ -37,23 +68,22 @@ function handleProgress() {
 function scrub(e) {
   const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
   video.currentTime = scrubTime;
-  console.log(e);
 }
 
-/* Hook up the event listners */
-video.addEventListener("click", togglePlay);
-video.addEventListener("play", updateButton);
-video.addEventListener("pause", updateButton);
-video.addEventListener("timeupdate", handleProgress);
+/* Hook up the event listeners */
+video.addEventListener('click', togglePlay);
+video.addEventListener('filter', filterButton);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
 
-toggle.addEventListener("click", togglePlay);
-skipButtons.forEach((button) => button.addEventListener("click", skip));
-ranges.forEach((range) => range.addEventListener("click", handleRangeUpdate));
-ranges.forEach((range) =>
-  range.addEventListener("mousemove", handleRangeUpdate)
-);
+toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
 let mousedown = false;
-progress.addEventListener("click", scrub);
-progress.addEventListener("mousemove", (e) => mousedown && scrub(e));
-progress.addEventListener("mousedown", () => (mousedown = true));
-progress.addEventListener("mouseup", () => (mousedown = false));
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
